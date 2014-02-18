6e20edabd518ab3cd5e3af623275cd9d5a901bfb
Dominik Schneider <schneider@jaando-software.de>
2014-02-18 22:14:04 +0100

    Added import of UserSettings File

--- pushover_Service/Setting.cs
+++ pushover_Service/Setting.cs
@@ -2,6 +2,8 @@ using System;
 using MonoDevelop.Core;
 using System.IO;
 
+////TODO: Notifying Object
+
 namespace FotoFridge.pushover_Service
 {
     /// <summary>
@@ -12,8 +14,9 @@ namespace FotoFridge.pushover_Service
         public static string UserID = String.Empty;
         public static string DeviceID = String.Empty;
         public static readonly string AppName = "FotoFridge";
-        public static readonly string AppDataFileName = "FotoFridgeSettings.xml";
+        public static readonly string AppDataFileName = "FotoFridge_PushoverSettings.xml";
 
+        public static Setting.SettingData Current;
 
         public Setting()
         {
@@ -26,10 +29,80 @@ namespace FotoFridge.pushover_Service
             string appDataFolder = Path.Combine(
                                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
                                        Setting.AppName);
+            if (!Directory.Exists(appDataFolder))
+            {
+                Directory.CreateDirectory(appDataFolder);
+            }
 
+            string appDataFilePath = Path.Combine(appDataFolder, Setting.AppDataFileName);
 
+            if (File.Exists(appDataFilePath))
+            {
+                using (StreamReader reader = new StreamReader(appDataFilePath))
+                {
+                    System.Xml.Serialization.XmlSerializer
+                    serializer = new System.Xml.Serialization.XmlSerializer(typeof(Setting.SettingData));
+
+                    Setting.Current = (SettingData)serializer.Deserialize(reader);
+                }
+            }
 
         }
+
+        /// <summary>
+        /// Provides User Settings Data
+        /// </summary>
+        public class SettingData
+        {
+            public SettingData(){}
+
+            private string userID;
+
+            /// <summary>
+            /// Gets or sets the pushover.net UserID
+            /// </summary>
+            /// <value>The user ID</value>
+            public string UserID
+            {
+                get
+                {
+                    if (this.userID == null)
+                    {
+                        this.userID = String.Empty;
+                    }
+                    return this.userID;
+                }
+                set
+                {
+                    this.userID = value;
+                }
+            }
+
+            private string userDevice;
+
+            /// <summary>
+            /// Gets or sets the users pushover.net device.
+            /// </summary>
+            /// <value>The user device name</value>
+            public string UserDevice
+            {
+                get
+                {
+                    if (this.userDevice == null)
+                    {
+                        this.userDevice = String.Empty;
+                    }
+                    return this.userDevice;
+                }
+                set
+                {
+                    this.userDevice = value;
+                }
+            }
+
+        }
+
+
 
     }
 }
